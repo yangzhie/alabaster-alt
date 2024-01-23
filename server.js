ddf51c3e944f73a0df78da387fcbe3d9bdf61f67
ArnavMalasi <arnavmalasi2212@gmail.com>
2024-01-23 20:13:43 +1100

    MVP working --- login WIP --- CSS styling WIP

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ const methodOverride = require("method-override");
 const db = require("./db/index");
 
 const homeRouter = require("./routes/home_router.js");
-
 const watchesRouter = require("./routes/watches_router.js");
-
 const objectsRouter = require("./routes/objects_router.js");
-
 const accessoriesRouter = require("./routes/accessories_router.js");
 
 app.set("view engine", "ejs");
